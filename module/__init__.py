60507bc992b6b1eae6158951078ded8cda5fc797
sushichaaaan <sushi_code@outlook.jp>
2022-02-12 08:21:33 +0900

    __init__.py

---
+++ module/__init__.py
@@ -0,0 +1,7 @@
+from module.client import *
+from module.http import *
+import module.model.artist
+import module.model.history
+import module.model.no_recent
+import module.model.song
+import module.model.stream
