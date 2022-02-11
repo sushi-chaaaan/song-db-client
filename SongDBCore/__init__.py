dcdda3e4b00f643b8e3c80ad598d162add4f5946
sushichaaaan <sushi_code@outlook.jp>
2022-02-12 08:34:44 +0900

    test

--- SongDBCore/__init__.py
+++ SongDBCore/__init__.py
@@ -1,6 +1,3 @@
 from SongDBCore.client import *
 from SongDBCore.http import *
-from SongDBCore.model.artist import *
-from SongDBCore.model.history import *
-from SongDBCore.model.song import *
-from SongDBCore.model.stream import *
+
---
+++ SongDBCore/model/__init__.py
@@ -0,0 +1,5 @@
+from SongDBCore.model.artist import *
+from SongDBCore.model.history import *
+from SongDBCore.model.no_recent import *
+from SongDBCore.model.song import *
+from SongDBCore.model.stream import *
